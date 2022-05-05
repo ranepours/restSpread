652a49ff0acabaa07c44a84da1ec1d5de5888c2f
ranepours <ranepours@gmail.com>
2022-05-05 18:44:16 -0400

    rest n spread hw

---
+++ restSpread.js
@@ -0,0 +1,41 @@
+filterOutOdds = (...argument) => {
+    return argument.filter(num => num % 2 === 0);
+}
+
+findMin = (...nums) => {
+    return Math.min(...nums);
+}
+
+mergeObjects = (obj, objAgain) => {
+    return {...obj, ...objAgain};
+}
+
+doubleAndReturn = (arr, ...argument) => {
+    return [...arr, ...argument.map((value) => value * 2 )];
+}
+
+removeItems = (items) => {
+    let random = Math.floor(Math.random() * items.length);
+    return [...items.slice(0, random), ...items.slice(random + 1)];
+    //getting where to slice was the HARDEST part of all of this lol
+}
+
+extend = (array1, array2) => {
+    return [...array1, ...array2];
+}
+
+addKeyVal = (obj, key, val) => {
+    return {...obj, [key]: val};
+}
+
+removeKey = (obj, key) => {
+    return obj = {[key]: undefined, ...obj };
+}
+
+combine = (obj1, obj2) => {
+    return {...obj1, ...obj2};
+}
+
+update = (obj, key, val) => {
+    return {...obj, key: [val]};
+}
